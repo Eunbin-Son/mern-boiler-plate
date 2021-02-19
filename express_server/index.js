793f2fb0c9db8b47d512af6b5149c80a040a004e
thswlsqls <thswlsqls@bible.ac.kr>
2021-02-19 16:20:06 +0900

    commit changes, connect mongoDB

--- express_server/index.js
+++ express_server/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
+const config = require("./config/dev");
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI,
+    {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true, 
+        dbName: "DB0"
+    })
+        .then(() => console.log('MongoDB Atlas survey program cluster0 is connected..'))
+        .catch(err => console.log('MongoDB Atlas is not connected, err ocuured : '+err));
+
 const port = 5001;
 
+
+
 app.listen(port, ()=> {
-    console.log(`express_server is now running at port ${port}`);
+    console.log(`Express_Server is now running at port ${port}`);
 });
